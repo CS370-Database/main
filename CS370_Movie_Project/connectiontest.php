114caceac9b370196390ce181797b1eb458f7f05
DMeier204 <131905811+DMeier204@users.noreply.github.com>
2023-05-01 23:26:15 -0500

    Delete connectiontest.php

--- CS370_Movie_Project/connectiontest.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = @mysqli_connect("localhost", "root", "password", "movie_db");
-if(mysqli_connect_errno()) {
-    echo "Failed XD" . "<br/>";
-}
-echo "Done!" . "<br/>";
-?>
-<?php include_once("header.php");?>
